723363c12667d785852bb8cab0883a3b55a1835d
Mustafa Karakaş <mustafakarakas61ts862@gmail.com>
2022-08-01 17:17:34 +0300

    Update DefUsersRepository.java
    @Repository eklendi.

--- src/main/java/com/mtm/test/mustafa/repository/DefUsersRepository.java
+++ src/main/java/com/mtm/test/mustafa/repository/DefUsersRepository.java
@@ -3,6 +3,7 @@ package com.mtm.test.mustafa.repository;
 import com.mtm.test.mustafa.entity.DefUsers;
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface DefUsersRepository extends CrudRepository<DefUsers, Integer> {
     public DefUsers findByNameAndId(String name,String id);
 }
